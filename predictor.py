1a09e931b3424d2c15c71c9d50e581774c05a2e8
Mathias <mathias.moog@outlook.de>
2025-09-24 23:17:32 +0200

    Fixed tuple line breaks

--- predictor.py
+++ predictor.py
@@ -1,14 +1,11 @@
 class Predictor:
-    DRAW_THRESHOLD = 2.25  # Not to be used in knock out phase if result after extra time + penalties is used in the rules
-    BIG_WIN_THRESHOLD = 7.5  # 5.25 for group stage, 8 for knockout?
-    HUGE_WIN_THRESHOLD = 18  # 18-20 seems about right for a 3:0
-    WIN = (
-        2,
-        1,
-    )  # (2,1) for group stage, (1,0) for knockout stage # TODO: detect if knockout
-    HUGE_WIN = (3, 0)
-    BIG_WIN = (2, 0)
-    DRAW = (0, 0)  # 0:0 is used because the most realistic scenario for a draw is that both teams do not attack since a draw is sufficient for both (see Schande von Gijon)
+    DRAW_THRESHOLD: float = 2.25  # Not to be used in knock out phase if result after extra time + penalties is used in the rules
+    BIG_WIN_THRESHOLD: float  = 7.5  # 5.25 for group stage, 8 for knockout?
+    HUGE_WIN_THRESHOLD: float  = 18  # 18-20 seems about right for a 3:0
+    WIN: tuple[int, int] = (2, 1)  # (2,1) for group stage, (1,0) for knockout stage # TODO: detect if knockout
+    HUGE_WIN: tuple[int, int] = (3, 0)
+    BIG_WIN: tuple[int, int] = (2, 0)
+    DRAW: tuple[int, int] = (0, 0)  # 0:0 is used because the most realistic scenario for a draw is that both teams do not attack since a draw is sufficient for both (see Schande von Gijon)
 
     # TODO: Calculate it based on the mode (no_bias = 1; away_bias = (6/4 + 5/3 + 4/2) / 3)
     # TODO: https://www.kicktipp.de/sparta2526/spielregeln Get the points directly from kicktipp
@@ -19,7 +16,7 @@ class Predictor:
     # Auswärtssieg	4	    5	            6
 
     # This is only used as a fallback if it cannot be parsed from the website
-    POINTS = {
+    POINTS: dict[str, dict[str, list[int]]] = {
         "no_bias": {
             "home": [4, 3, 2],
             "away": [4, 3, 2],
@@ -32,7 +29,7 @@ class Predictor:
         }
     }
 
-    def __init__(self, mode="no_bias", points=None):
+    def __init__(self, mode: str = "no_bias", points: dict[str, list[int]] = None):
         self.mode = mode
         if points is None:
             self.points = self.POINTS[mode]
